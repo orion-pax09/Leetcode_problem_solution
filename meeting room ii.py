c3943ada6d497b38d7c7b51a8c579f32cfc45932
Muhammad Hamza Khan <khumza050@gmail.com>
2025-09-04 17:38:32 +0500

    feat:feat: implement meeting room II algorithm

---
+++ meeting room ii.py
@@ -0,0 +1,17 @@
+import heapq
+def NMEETINGROOMii(interval):
+    Interval=sorted(interval,key=lambda x: x[0])
+    heap=[]
+    heapq.heappush(heap,Interval[1][0])
+    for s in range(1,len(interval)):
+        start,end=Interval[s]
+        if start >= heap[0]:
+            heapq.heappop(heap)
+        heapq.heappush(heap,end)
+    return len(heap)
+nput=NMEETINGROOMii([[2,3],[4,6],[7,9],[2,5],[3,6],[5,9],[4,7]])
+print(nput)
+    
+
+
+
